fcd3bf5bc853c76bcdfbfde6a7a3c2fa2477dc6e
zzz6866 <zzz6866@gmail.com>
2022-12-11 15:31:48 +0900

    테라폼 ssh(remote_exec) command 테스트

--- terraform/README.md
+++ terraform/README.md
@@ -3,6 +3,9 @@
 - K3D 환경 [Link](https://github.com/k3d-io/k3d)
 - SSH 원격 클러스터 구성
 
+## 단계
+- 생성 후 삭제까지 진행
+
 ## 참고
 - https://github.com/k3d-io/k3d
 - https://gitlab.com/bengordonfraser/terraform-k3d
--- terraform/k3d-tf-cluster.tf
+++ terraform/k3d-tf-cluster.tf
@@ -1,6 +1,4 @@
-resource "null_resource" "k3d-cluster" {
-  # ...
-
+resource "null_resource" "cluster_create" {
   # Establishes connection to be used by all
   # generic remote provisioners (i.e. file/remote-exec)
   connection {
@@ -11,15 +9,21 @@ resource "null_resource" "k3d-cluster" {
     timeout     = "2m"
   }
 
+  for_each = toset(var.k3d_cluster_name)
   provisioner "remote-exec" {
     inline = [
-      "echo asdfasdf",
+      "k3d cluster create ${each.key} \\",
+      "--agents ${var.agent_count} \\",
+      "--servers ${var.server_count} \\",
+      "--api-port ${var.k3d_cluster_ip}:${var.k3d_cluster_port} \\",
+      "--port ${var.k3d_host_lb_port}:${var.k3d_cluster_lb_port}@loadbalancer \\",
+      "--image rancher/k3s:${var.k3s_version}",
     ]
   }
 
   provisioner "remote-exec" {
     inline = [
-      "ps -ef | tail -n 10",
+      "k3d cluster list #${each.key}",
     ]
   }
 
@@ -35,3 +39,27 @@ resource "null_resource" "k3d-cluster" {
   #    ]
   #  }
 }
+
+resource "null_resource" "cluster_delete" {
+  connection {
+    user        = "k3d"
+    host        = "k3d-cluster"
+    type        = "ssh"
+    private_key = file("~/.ssh/id_rsa")
+    timeout     = "2m"
+  }
+
+  for_each = toset(var.k3d_cluster_name)
+  provisioner "remote-exec" {
+    inline = ["k3d cluster delete ${each.key}"]
+    when    = destroy
+  }
+}
+
+#data "docker_network" "cluster_create" {
+#  for_each = toset(var.k3d_cluster_name)
+#  depends_on = [
+#    null_resource.cluster_create
+#  ]
+#  name = "k3d-${each.key}"
+#}
